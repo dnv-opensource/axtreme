54184cf11cbf490682c274e43f9f16346dd4cecf
am-kaiser <63399571+am-kaiser@users.noreply.github.com>
2025-08-05 15:27:32 +0200

    addressed review comments

--- tests/qoi/test_marginal_cdf_extrapolation_with_importance_sampling.py
+++ tests/qoi/test_marginal_cdf_extrapolation_with_importance_sampling.py
@@ -4,24 +4,22 @@ Tests for MarginalCDFExtrapolation with importance sampling.
 Plan:
     - Unit test:
         - _parameter_estimates: input and output weights are attached to the same samples
-    - Integration test:
-        - successful run of minimal version of the qoi using a deterministic model with importance sampling
     - System Test
-        - Show that that the QoI estimation requires less samples with importance samples compared to when using
-        the regular approach of including the whole environment dataset.
+        - Show that that the QoI estimation with importance samples has less uncertainty in the result compared to when
+        using the regular approach of including the whole environment dataset.
 """
 
 # %%
 import json
 from pathlib import Path
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import pytest
 import torch
 from botorch.models.deterministic import GenericDeterministicModel
 from botorch.sampling.index_sampler import IndexSampler
-from numpy.typing import NDArray
 from torch.distributions import MultivariateNormal
 from torch.utils.data import DataLoader
 
@@ -32,127 +30,85 @@ from axtreme.qoi import MarginalCDFExtrapolation
 
 def test_parameter_estimates_consistency_of_weights(gp_passthrough_1p: GenericDeterministicModel):
     """
-    Run a minimal version of the qoi using a deterministic model and a short period_len to test that the importance
-    weights are still connected to the correct samples when using MarginalCDFExtrapolation._parameter_estimates.
+    Tests that the `_parameter_estimates` method in `MarginalCDFExtrapolation` correctly combines environment samples
+    and their associated importance weights, and that the outputs match expected shapes and values when using a
+    deterministic Gaussian Process (GP). The deterministic nature of the GP model leads to fully predictable posterior
+    samples, i.e. they are identical to the environment samples.
+
+    This test ensures that:
+      - Posterior samples returned by `_parameter_estimates` are correctly shaped and ordered.
+      - Importance weights are preserved and correctly matched to the associated samples.
 
     Args:
         gp_passthrough_1p is defined in conftest.py. It creates a deterministic GP which always produces identical
         posterior samples. The output location is a direct pass through of the given input data, and the scale is set
         to 1e-6.
-
-    Notes:
-    -   _parameter_estimates needs the importance dataset with specific dimensions by using ImportanceAddedWrapper and
-        DataLoader the correct format is ensured and in addition it can be tested that these two functions do not change
-        the relation between samples and weights.
     """
-    # Define simple importance samples and weights
-    importance_samples = torch.Tensor([[1], [2], [3], [4]])
-    importance_weights = torch.Tensor([0.1, 0.2, 0.3, 0.4])
-
-    # Wrap them together
-    importance_dataset = ImportanceAddedWrapper(MinimalDataset(importance_samples), MinimalDataset(importance_weights))
-
-    # The batch size should be smaller than the number of input samples to ensure that the importance_dataset gets
-    # split up into smaller batches. By doing this we can test that the batching in _parameter_estimates does not mess
-    # up the relation between samples and weights.
-    dataloader = DataLoader(importance_dataset, batch_size=2)
+    # MarginalCDFExtrapolation expects an iterable of env data. To use importance samples, each item is expected to be
+    # of the following form [env_samples, importance_weights], where:
+    # env_samples.shape = (batch_size,d), and importance_weights.shape = (batch_size,)
+    # Note: in practice a dataloader is typically used to achieve this.
+    env_and_importance_data = [
+        # data batch 1: [env_samples, importance_weights]
+        [torch.tensor([[1.0], [2.0]]), torch.Tensor([0.1, 0.2])],
+        # data batch 2: [env_samples, importance_weights]
+        [torch.tensor([[3.0], [4.0]]), torch.Tensor([0.3, 0.4])],
+    ]
 
     # Run the method
     qoi_estimator = MarginalCDFExtrapolation(
-        env_iterable=dataloader,
-        period_len=10,
-        posterior_sampler=IndexSampler(torch.Size([1])),  # draw 1 posterior samples to simplify comparison
-        quantile=torch.tensor(0.5, dtype=torch.float64),
-        quantile_accuracy=torch.tensor(0.1, dtype=torch.float64),
+        env_iterable=env_and_importance_data,
+        period_len=int(float("nan")),  # not used in this test
+        posterior_sampler=IndexSampler(torch.Size([1])),  # draw 1 posterior sample to simplify comparison
+        quantile=torch.tensor(float("nan"), dtype=torch.float64),  # not used in this test
+        quantile_accuracy=torch.tensor(float("nan"), dtype=torch.float64),  # not used in this test
         dtype=torch.float64,
     )
 
     # Get posterior samples and connected weights for given QoI estimator
     posterior_samples, importance_weights_qoi = qoi_estimator._parameter_estimates(gp_passthrough_1p)
 
+    # Verify that the output has the correct dimensions
+    # For the posteriors samples the expected shape is: (n_posterior_samples, n_env_samples, n_targets)
+    # with n_targets being the number of targets the GP should predict. For gp_passthrough_1p this is set to 2.
+    assert posterior_samples.shape == torch.Size([1, 4, 2])
+    # For the weights the expected shape is: (n_posterior_samples, n_env_samples)
+    assert importance_weights_qoi.shape == torch.Size([1, 4])
+
     # As a deterministic GP is used the posterior samples should not only have the same values as the
     # input samples but their order should be the same as well
-    assert torch.equal(torch.flatten(importance_samples), posterior_samples[..., 0][0])
-    assert torch.equal(torch.flatten(importance_weights), torch.flatten(importance_weights_qoi))
-
-
-def test_qoi_runs_with_importance_sampling(gp_passthrough_1p: GenericDeterministicModel):
-    """
-    Run a minimal version of the qoi using a deterministic model and a short period_len to test that it successfully
-    runs with importance sampling.
-
-    Args:
-        gp_passthrough_1p is defined in conftest.py. It creates a deterministic GP which always produces identical
-        posterior samples. The output location is a direct pass through of the given input data, and the scale is set
-        to 1e-6.
-
-    Notes:
-    -   The difference to test_parameter_estimates_consistency_of_weights is that the model is passed directly
-        to the QoI estimator not qoi_estimator._parameter_estimates.
-    """
-    # Define simple importance samples and weights
-    importance_samples = torch.Tensor([[1], [2], [3], [4]])
-    importance_weights = torch.Tensor([0.1, 0.2, 0.3, 0.4])
-
-    # Wrap them in a dataloader
-    importance_dataset = ImportanceAddedWrapper(MinimalDataset(importance_samples), MinimalDataset(importance_weights))
-    dataloader = DataLoader(importance_dataset, batch_size=10)
-
-    # Run the method
-    qoi_estimator = MarginalCDFExtrapolation(
-        env_iterable=dataloader,
-        period_len=10,
-        posterior_sampler=IndexSampler(torch.Size([1])),  # draw 1 posterior samples to simplify comparison
-        quantile=torch.tensor(0.5, dtype=torch.float64),
-        quantile_accuracy=torch.tensor(0.1, dtype=torch.float64),
-        dtype=torch.float64,
+    assert torch.equal(
+        torch.cat([batch[0] for batch in env_and_importance_data]).flatten(), posterior_samples[..., 0][0]
     )
-
-    # This only tests if the functions runs successfully and does not concern itself with the output
-    _ = qoi_estimator(gp_passthrough_1p)
-
-
-# These are helper functions to define the loc and scale which are used in the simulator used for the system test
-def _true_loc_func(x: NDArray[np.float64]) -> NDArray[np.float64]:
-    # For this toy example we use a MultivariateNormal distribution
-    dist_mean, dist_cov = torch.tensor([1, 1]), torch.tensor([[0.03, 0], [0, 0.03]])
-
-    dist = MultivariateNormal(loc=dist_mean, covariance_matrix=dist_cov)
-
-    return np.exp(dist.log_prob(torch.tensor(x)).numpy())
-
-
-def _true_scale_func(x: NDArray[np.float64]) -> NDArray[np.float64]:
-    # For this toy example we use a constant scale for simplicity
-    return np.ones(x.shape[0]) * 0.1
+    assert torch.equal(
+        torch.cat([batch[1] for batch in env_and_importance_data]).flatten(), torch.flatten(importance_weights_qoi)
+    )
 
 
 # Ruff does not allow default arguments in test functions. Using a decorator circumvents that.
-@pytest.mark.parametrize("params", [(1, False)])
+@pytest.mark.parametrize("error_tolerance, visualize", [(1, False)])
 def test_system_marginal_cdf_with_importance_sampling(
-    params: tuple[float, bool],
-) -> tuple[pd.DataFrame, float] | None:
+    error_tolerance: float,
+    *,
+    visualize: bool,
+):
     """
-    Show that that the QoI estimation requires less samples with importance samples compared to when using
-    the regular approach of including the whole environment dataset.
+    There is variability in the specific samples in the env_data used to instantiate a QoiEstimator, which creates the
+    variability of the QoIEstimator estimate. This can be seen by inspecting how the estimates given change when the
+    QoiEstimator has been instantiated with a different dataset. This test shows how (good) importance samples can
+    reduce the variability between estimates of QoIEstimators instantiated with different env_data. The effect of GP
+    uncertainty is removed by using a deterministic model, meaning all uncertainty in the estimate comes from the
+    environment samples.
 
     Args:
-        params: tuple of
-            - The error allowed in assertions is multiplied by this number.
-            - Bool to specify if the QoI results shall be returned for further testing or plotting.
-
-    Returns:
-        If return_results==True: the QoI results are returned in a pandas Dataframe
-
-    A deterministic GP is used to remove most of the uncertainty related to the GP. Some uncertainty remains as the GP
-    is not fit on the whole dataset.
+        error_tolerance: The error allowed in assertions is multiplied by this number.
+        visualize: Bool to specify if the QoI results shall be plotted.
 
     Expectation:
     As we use a deterministic GP the QoI estimator will not be a distribution but a point representing the mean.
     The std of the means of several runs of the QoI estimator should be lower with uncertainty sampling than without.
     By visual inspection a threshold for the std for importance sampling is chosen.
     """
-    error_tolerance, return_results = params
 
     # Load precalculated importance samples and weights
     importance_samples = torch.load(
@@ -175,12 +131,20 @@ def test_system_marginal_cdf_with_importance_sampling(
         brute_force_qoi = json.load(file)["statistics"]["median"]
 
     # Set up a deterministic GP which uses the true underlying functions of loc and scale used in the simulator
-    def true_underlying_func(x: torch.Tensor) -> torch.Tensor:
-        locs = torch.from_numpy(_true_loc_func(x.numpy())).unsqueeze(-1)
-        scales = torch.from_numpy(_true_scale_func(x.numpy())).unsqueeze(-1)
-        return torch.concat([locs, scales], dim=-1)
+    def _true_underlying_func(x: torch.Tensor) -> torch.Tensor:
+        # For this toy example we use a MultivariateNormal distribution
 
-    gp_deterministic = GenericDeterministicModel(true_underlying_func, num_outputs=2)
+        # Define true loc function
+        dist_mean, dist_cov = torch.tensor([1, 1]), torch.tensor([[0.03, 0], [0, 0.03]])
+        dist = MultivariateNormal(loc=dist_mean, covariance_matrix=dist_cov)
+        loc = torch.exp(dist.log_prob(x))
+
+        # Define true scale function
+        scale = torch.ones(x.shape[0]) * 0.1
+
+        return torch.stack([loc, scale], dim=-1)
+
+    gp_deterministic = GenericDeterministicModel(_true_underlying_func, num_outputs=2)
 
     # Create jobs with with and without importance sampling
     qoi_jobs = []
@@ -221,8 +185,8 @@ def test_system_marginal_cdf_with_importance_sampling(
     df_jobs = pd.json_normalize([item.to_dict() for item in qoi_results], max_level=1)
     df_jobs.columns = df_jobs.columns.str.removeprefix("tags.")
 
-    # The aim of this test is to show that QoI estimation with importance sampling requires less samples
-    # (i.e. num_samples) to converge to the solution than with using the full environment dataset.
+    # The aim of this test is to show that QoI estimation with importance sampling results in less uncertainty in the
+    # QoI compared to when using the full environment dataset.
     # All absolute values in the following asserts are chosen based on visual inspection of the QoI results.
     # There are two criteria we use to judge the convergence:
     # 1. the mean of the QoI means is close to the brute force solution and in particular the one with
@@ -239,44 +203,33 @@ def test_system_marginal_cdf_with_importance_sampling(
     # 2. the std of the QoI means is small with importance sampling
     assert df_jobs.loc[df_jobs["dataset_name"] == "importance_sample", "mean"].std() <= 0.25 * error_tolerance
 
-    if return_results:
-        return df_jobs, brute_force_qoi
+    if visualize:
+        _, ax = plt.subplots(1, 2, figsize=(10, 5), sharex=True, sharey=True)
 
-    return None
+        # Plot results for full env data
+        df_jobs[df_jobs["dataset_name"] == "full"].hist(column="mean", ax=ax[0], grid=False)
+        ax[0].axvline(brute_force_qoi, c="orange", label=f"Brute force ({brute_force_qoi:.2f})")
+        ax[0].set_title(
+            "Dataset: full, "
+            f"mean={df_jobs.loc[df_jobs['dataset_name'] == 'full', 'mean'].mean():.2f}, "
+            f"std={df_jobs.loc[df_jobs['dataset_name'] == 'full', 'mean'].std():.2f}"
+        )
+        ax[0].legend()
+
+        # Plot results for importance sampling
+        df_jobs[df_jobs["dataset_name"] == "importance_sample"].hist(column="mean", ax=ax[1], grid=False)
+        ax[1].axvline(brute_force_qoi, c="orange", label=f"Brute force ({brute_force_qoi:.2f})")
+        ax[1].set_title(
+            "Dataset: importance sample, "
+            f"mean={df_jobs.loc[df_jobs['dataset_name'] == 'importance_sample', 'mean'].mean():.2f}, "
+            f"std={df_jobs.loc[df_jobs['dataset_name'] == 'importance_sample', 'mean'].std():.2f}"
+        )
+        ax[0].legend()
 
 
 # %% Can be used to get plots for test_system_marginal_cdf_with_importance_sampling
 if __name__ == "__main__":
-    # %% Calculate QoI
-    from typing import cast
-
-    qoi_results, brute_force_qoi = cast(
-        "tuple[pd.DataFrame, float]", test_system_marginal_cdf_with_importance_sampling((1, True))
-    )
-
-    # %% Plot results
-    import matplotlib.pyplot as plt
-
-    fig, ax = plt.subplots(1, 2, figsize=(10, 5), sharex=True, sharey=True)
-
-    # Plot results for full env data
-    qoi_results[qoi_results["dataset_name"] == "full"].hist(column="mean", ax=ax[0], grid=False)
-    ax[0].axvline(brute_force_qoi, c="orange", label=f"Brute force ({brute_force_qoi:.2f})")
-    ax[0].set_title(
-        "Dataset: full, "
-        "mean={qoi_results.loc[qoi_results['dataset_name'] == 'full', 'mean'].mean():.2f}, "
-        "std={qoi_results.loc[qoi_results['dataset_name'] == 'full', 'mean'].std():.2f}"
-    )
-    ax[0].legend()
-
-    # Plot results for importance sampling
-    qoi_results[qoi_results["dataset_name"] == "importance_sample"].hist(column="mean", ax=ax[1], grid=False)
-    ax[1].axvline(brute_force_qoi, c="orange", label=f"Brute force ({brute_force_qoi:.2f})")
-    ax[1].set_title(
-        "Dataset: importance sample, "
-        "mean={qoi_results.loc[qoi_results['dataset_name'] == 'importance_sample', 'mean'].mean():.2f}, "
-        "std={qoi_results.loc[qoi_results['dataset_name'] == 'importance_sample', 'mean'].std():.2f}"
-    )
-    ax[0].legend()
+    # %%
+    test_system_marginal_cdf_with_importance_sampling(1, visualize=True)
 
 # %%
