8cd0b9d96683969eb45c4ef11e8484475193072b
am-kaiser <63399571+am-kaiser@users.noreply.github.com>
2025-10-10 10:08:51 +0200

    shortened docstring to key info

--- src/axtreme/sampling/importance_sampling.py
+++ src/axtreme/sampling/importance_sampling.py
@@ -1,5 +1,5 @@
 # %%
-"""This module provides methods to create importance samples and weights if the env data distribution is known.
+"""This module provides methods to create importance samples and weights.
 
 Importance sampling focuses computational effort on regions of interest. This is especially useful when only a small
 part of the environment data contributes meaningfully to the quantity of interest (QoI).
@@ -7,6 +7,9 @@ part of the environment data contributes meaningfully to the quantity of interes
 In this file the following is included:
 - Create importance sample and weights for a given importance distribution.
 - Create importance sample and weights for a uniform region.
+
+See docs/source/technical_details/importance_sampling.md and tutorials/importance_sampling.py for examples and guidance
+on when and how to use importance sampling.
 """
 
 from collections.abc import Callable
@@ -17,8 +20,6 @@ from torch.distributions.distribution import Distribution
 torch.set_default_dtype(torch.float64)
 
 
-# TODO(sw25-05-26): make the docstring here a bit clearer. Bit more explanation about the input functions.
-# Maybe change the order
 def importance_sampling_from_distribution(
     env_distribution_pdf: Callable[[torch.Tensor], torch.Tensor],
     importance_distribution_pdf: Callable[[torch.Tensor], torch.Tensor],
@@ -27,16 +28,8 @@ def importance_sampling_from_distribution(
 ) -> tuple[torch.Tensor, torch.Tensor]:
     """Perform importance sampling for a given importance distribution.
 
-    This function generates samples from a given importance sampling distribution and computes weights
-    that accounts for the relation between the true environment distribution and the importance distribution, i.e.,
-
-    - Suppose p(x) is the probability density function (PDF) of the true environment.
-    - Suppose h(x) is the PDF of the importance sampling distribution.
-    - Then, for each sample x, the importance sampling weight is: w(x) = p(x) / h(x).
-
     Args:
-        env_distribution_pdf: The pdf function of the real environment distribution. This can either be a known function
-            or for example be estimated based on the environment data using KDE (kernel density estimation).
+        env_distribution_pdf: The pdf function of the real environment distribution.
         importance_distribution_pdf: The pdf function of the importance sampling distribution.
         importance_sampling_sampler: The sampler for producing samples from the importance sampling distribution.
         num_samples: The number of samples to generate.
@@ -47,8 +40,7 @@ def importance_sampling_from_distribution(
 
     Notes:
         A theoretical requirement for the importance sampling distribution is that it should be non-zero where the real
-        environment distribution is non-zero. This means that: p(x) != 0 => h_x(x) != 0.
-        However, in practice, this requirement is often relaxed.
+        environment distribution is non-zero. However, in practice, this requirement is often relaxed.
     """
     samples = importance_sampling_sampler(torch.Size([num_samples]))
     pdf = env_distribution_pdf(samples)
@@ -58,21 +50,23 @@ def importance_sampling_from_distribution(
     return samples, weights
 
 
-# TODO(sw25-05-26): make the docstring here a bit clearer
 def importance_sampling_distribution_uniform_region(
     env_distribution_pdf: Callable[[torch.Tensor], torch.Tensor],
     region: torch.Tensor | tuple[torch.Tensor, torch.Tensor],
     threshold: float,
     num_samples_total: int,
 ) -> tuple[torch.Tensor, torch.Tensor]:
-    """Generates the importance sampling samples and weights by sampling uniformly from a defined region of interest.
+    """Generates importance samples and weights by uniformly sampling a region and filtering by env-pdf > threshold.
 
-    This method is useful when the importance distribution is unknown. The method constructs an implicit importance
-    distribution by:
+    The method constructs an implicit importance distribution by:
     1. Sampling uniformly in a broad region.
-    2. Filtering samples based on whether their environment PDF exceeds a threshold.
-    3. Estimating a uniform importance distribution over the filtered subset.
-    4. Calculating weights as w(x) = p(x) / h(x), where h(x) is approximated analytically.
+    2. Filtering samples based on whether their environment PDF (p(x)) exceeds a threshold.
+    3. Estimating a uniform importance distribution, h(x), over the filtered subset.
+    4. Calculating weights as w(x) = p(x) / h(x).
+
+    The mathematical justification for this algorithm is given in:
+    "Efficient Long-Term Structural Reliability Estimation with Non-Gaussian Stochastic Models: A Design of Experiments
+    Approach.” arXiv, March 3, 2025. https://doi.org/10.48550/arXiv.2503.01566."
 
     Args:
         env_distribution_pdf: The pdf function of the real environment distribution.
@@ -90,7 +84,7 @@ def importance_sampling_distribution_uniform_region(
           - both with shape: (d,)
 
         threshold: Environment regions with pdf values less than this threshold will not be explored by the importance
-            samples. See `Details` for more information on how this threshold is used.
+            samples.
 
         num_samples_total: Total number of samples to return.
 
@@ -99,59 +93,12 @@ def importance_sampling_distribution_uniform_region(
             The filtered samples drawn from the uniform distribution. Shape (num_samples_total,d)
             Importance sampling weights for each sample. Shape (num_samples_total,)
 
-    Details:
-        The mathematical justification for this algorithm is given in
-
-        "Efficient Long-Term Structural Reliability Estimation with Non-Gaussian
-        Stochastic Models: A Design of Experiments Approach.” arXiv, March 3, 2025.
-        https://doi.org/10.48550/arXiv.2503.01566."
-
-        The algorithm works as follows:
-        1. For a chosen `threshold` c for the real environment distribution.
-
-           1.1 Define a sub-region F = {x: p(x) > c}, where p(x) is the probability density function
-                of the real environment distribution. This should contain the significant regions of p(x).
-
-        2. Generate `num_samples_total` uniform samples from the region. The region must cover all of F.
-
-           2.1 Discard any points not in F. `num_samples` is the number of points that are left after discarding.
-           2.2. If `num_samples` is less than `num_samples_total`, repeat step 2 until enough samples are generated.
-
-        3. The PDF of the sampled points `h_x(x)` is a uniform distribution over the region F.
-
-           3.1 `h_x(x)` is estimated with `1/volume(region)`.
-
-        4. The importance sampling weights are then calculated as w(x) = p(x)/h_x(x)
-
-        This importance sampling approach is useful when:
-            - We want to find a tighter/smaller region for our problem automatically. This approach can also
-            automatically create non-linear boundaries.
-            - You may have disjoint regions.
-            - Need the impact of rare events to be explored.
-
-        The approach is sensitive to the following parameters. Often trial and and error (and visualisation) is required
-        to find an appropriate combination:
-        - `threshold`:
-
-          - Too low: Areas of the environment space that are practically impossible (and therefore irrelevant) will be
-            included. This will create pointless additional computation.
-          - Too high: Areas of the environment space that are important for down stream calculations may be excluded.
-            (e.g rare but important event). Calculations done with this dataset could then give incorrect results.
-
-        - `region`:
-
-          - Too large: num_samples_total will need to be very large in order to get enough samples in F.
-          - Too small: Areas of the environment space that are important for down stream calculations may be excluded.
-            (e.g rare but important event). Calculations done with this dataset could then give incorrect results.
-
-        The following check can help determine the suitability of you parameters:
-        - Plot and check with a domain expert that the important regions of space are in F. Use a larger F if unsure.
-        - Ensure that you are left with enough samples in F.
-
-        Notes: This implementation breaks the importance sampling assumption that `p(x_i) != 0 => h_x(x_i) != 0`.
-            - This still produces an exact result when responses/downstream-task [lets call it r(x_i)] is 0.
-                - importance_result = sum[ r(x_i) * p(x_i)/h_x(x_i)] where x ~ h_x(x)
-                - The point would add 0 to the non-importance weighted sum.
+    Notes:
+        This implementation breaks the importance sampling assumption that `p(x_i) != 0 => h_x(x_i) != 0`.
+        While h_x is non-zero everywhere in the defined region, it is zero outside of it.
+        - This still produces an exact result when responses/downstream-task [lets call it r(x_i)] is 0.
+            - importance_result = sum[ r(x_i) * p(x_i)/h_x(x_i)] where x ~ h_x(x)
+            - The point would add 0 to the non-importance weighted sum.
             - It will produce an approximate result if r(x_i) != 0.
                 - This is a reasonable approximation if p(x_i) is considered to be close enough to 0.
     """
